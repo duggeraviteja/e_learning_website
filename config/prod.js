b740d9e136fc44e2d3d0caeedb2317849fe3d6bb
unknown <you@example.com>
2022-01-29 01:25:15 +0530

    first

--- config/prod.js
+++ config/prod.js
@@ -1,11 +1,14 @@
+
+
 module.exports = {
-    PORT:"3001",
-    DB:"mongodb+srv://admin-ravi:ravi_teja@7899@cluster0.jcypu.mongodb.net/USERS?retryWrites=true&w=majority",
-    JWT_SECRET:"LETSHOPESOMETHINGGOODISABOUTCOMELETSGOLETSSEETTHENEXT",
-    SENDGRID_KEY:"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",
-    GOOGLE_CLIENT_ID:"192103012114-k0tjg6b1i45mc91en74j6d54040mid5e.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET:"SMEla05qFsoSlVj7i87YT_98",
-    CLOUD_NAME:"raviteja-cloud",
-    CLOUD_KEY:"252662546289542",
-    CLOUD_SECRET:"h_fkeIQkAmM3oy3R4l9g-MVhkS8",
+    PORT: process.env.PORT,
+    DB: process.env.DB,
+    JWT_SECRET: process.env.JWT_SECRET,
+    SENDGRID_KEY: process.env.SENDGRID_KEY,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    CLOUD_NAME: process.env.CLOUD_NAME,
+    CLOUD_KEY: process.env.CLOUD_KEY,
+    CLOUD_SECRET: process.env.CLOUD_SECRET,
+
 }
